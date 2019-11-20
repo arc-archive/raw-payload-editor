2ba05a8b00b61181c8658e8503734186e2ac4088
Pawel <jarrodek@gmail.com>
2019-11-20 10:21:02 -0800

    test: removing Edge from tests until Jan 2020 release

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -15,15 +15,17 @@ module.exports = (config) => {
     }
   });
   // if you want to change default browsers
-  // slConfig.browsers = [
-  //   'SL_Chrome',
-  //   'SL_Chrome-1',
-  //   'SL_Firefox',
-  //   'SL_Firefox-1',
-  //   'SL_Safari',
-  //   'SL_Safari-1',
-  //   'SL_EDGE'
-  // ];
+  slConfig.browsers = [
+    'SL_Chrome',
+    'SL_Chrome-1',
+    'SL_Firefox',
+    'SL_Firefox-1',
+    'SL_Safari',
+    'SL_Safari-1',
+    // The passing lineNumber test doesn't work in Edge, even when it is actually
+    // working when running demo on Edge. Disabling until January 2020 Edge release.
+    // 'SL_EDGE'
+  ];
   config.set(merge(createBaseConfig(config), slConfig));
   return config;
 };
