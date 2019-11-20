f687c493a85b2104294844a3ec18ebc7761b77f3
Pawel <jarrodek@gmail.com>
2019-11-20 10:14:41 -0800

    test: updating SL configuration

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -1,32 +1,29 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('deepmerge');
-const slSettings = require('@advanced-rest-client/testing-karma-sl/sl-settings.js');
+const { slSettings } = require('@advanced-rest-client/testing-karma-sl');
 const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
-  const cnf = slSettings();
-  cnf.sauceLabs.testName = 'raw-payload-editor';
-  cnf.browsers = [
-    'SL_Chrome',
-    'SL_Chrome-1',
-    'SL_Firefox',
-    'SL_Firefox-1',
-    'SL_Safari',
-    'SL_EDGE'
-  ];
-  if (process.env.TRAVIS) {
-    const buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
-
-    cnf.browserStack = {
-      build: buildLabel,
-      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
-    };
-
-    cnf.sauceLabs.build = buildLabel;
-    cnf.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
-  }
-
-  config.set(merge(createBaseConfig(config), cnf));
-
+  const slConfig = merge(slSettings(config), {
+    sauceLabs: {
+      testName: 'raw-payload-editor',
+    },
+    client: {
+      mocha: {
+        timeout : 6000
+      }
+    }
+  });
+  // if you want to change default browsers
+  // slConfig.browsers = [
+  //   'SL_Chrome',
+  //   'SL_Chrome-1',
+  //   'SL_Firefox',
+  //   'SL_Firefox-1',
+  //   'SL_Safari',
+  //   'SL_Safari-1',
+  //   'SL_EDGE'
+  // ];
+  config.set(merge(createBaseConfig(config), slConfig));
   return config;
 };
